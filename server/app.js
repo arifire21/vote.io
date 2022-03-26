52c7493406eb26db2f864ad02417aa7905bfbd75
Jake Taylor <jacobtaylor1@usf.edu>
2022-03-26 14:25:12 -0400

    shells of routes

--- server/app.js
+++ server/app.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+// app.get('/', function (req, res) {
+//     res.send('Hello!');
+// })
 
+// account creation
+app.post('/create-account', function (req, res) {
 
+})
+
+// votes
+app.get('/votes', function (req, res) {
+    res.send('votes page');
+})
+
+// vote
+app.get('/vote?id=', function (req, res) {
+    res.send('individual vote for id page');
+})
 
 app.listen(5000, ()=>{
     console.log('server started on port 5000')
