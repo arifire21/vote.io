83b9986a36387bddcc98de7b54592f8cf5ab9f7c
Andrew <andrewscaldwell@gmail.com>
2022-03-26 14:11:18 -0800

    adding more server boilerplate

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
 app.use(express.json())
+const {verifyConnection} = require('./utils/cockroach')
+
+verifyConnection()
 
 
 
