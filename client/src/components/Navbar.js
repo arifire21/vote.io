8f24d16fdbebbc7af81d13abdf0e35398ae3f32e
arifire21 <ariwolfpup@icloud.com>
2022-03-26 22:45:48 -0400

    ey look a navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function CreatePoll() {
+    return (
+        <div className="navbar-container">
+            <p>hi</p>
+            <p>hi</p>
+            <p>hi</p>
+
+        </div>
+    );
+}
