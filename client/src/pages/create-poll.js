fc8072952f17524919a36716f7b9a955477d3d8f
arifire21 <ariwolfpup@icloud.com>
2022-03-27 03:54:35 -0400

    page changes

--- client/src/pages/create-poll.js
+++ client/src/pages/create-poll.js
@@ -1,34 +1,63 @@
-import TestHeader from "../components/TestHeader";
+import React, {useState} from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Navbar from "../components/Navbar";
 
 export default function CreatePoll() {
-  // const 
+  const axios = require('axios');
+
+  const [title, setTitle] = useState('');
+  const titleChange = e => setTitle(e.target.value);
+  const [desc, setDesc] = useState('');
+  const descChange = e => setDesc(e.target.value);
+  const [opt1, setOpt1] = useState('');
+  const opt1Change = e => setOpt1(e.target.value);
+  const [opt2, setOpt2] = useState('');
+  const opt2Change = e => setOpt2(e.target.value);
+
+  // handleSubmit = event => {
+  //   event.preventDefault();
+
+  //   const user = {
+  //     title: this.state.title,
+  //     description: this.state.description,
+  //     option1: this.state.option1,
+  //     option2: this.state.option2,
+  //   };
+
+  //   axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+  //     .then(res => {
+  //       console.log(res);
+  //       console.log(res.data);
+  //     })
+  // }
 
     return (
       <div>
-        {/* <TestHeader/> */}
         <Navbar/>
 
           <h1>Create a Poll</h1>
-          <div className="form-container">
-            <div className="form-item">
-              <TextField id="create-title" variant="filled" label="Title" required="true"/>
+          <form>
+            <div className="form-container">
+              <div className="form-item">
+                <TextField id="create-title" variant="filled" label="Title" required="true" value={title} onChange={titleChange}/>
+              </div>
+              <div className="form-item">
+                <TextField id="create-desc" variant="filled" multiline="True" minRows="3" maxRows="4" label="Description" required="true" value={desc} onChange={descChange}/>
+              </div>
+              <div className="form-item">
+                <TextField id="create-opt1" variant="filled" label="Candidate 1" required="true" value={opt1} onChange={opt1Change}/>
+              </div>
+              <div className="form-item">
+                <TextField id="create-opt2" variant="filled" label="Candidate 2" required="true" value={opt2} onChange={opt2Change}/>
+              </div>
+              <div className="form-item submit-btn">
+                <span style={{ cursor: 'not-allowed' }}>
+                  <Button variant="contained" disabled={!title || !desc || !opt1 || !opt2 }>Submit</Button>
+                </span>
+              </div>
             </div>
-            <div className="form-item">
-              <TextField id="create-desc" variant="filled" multiline="True" minRows="3" maxRows="4" label="Description" required="true"/>
-            </div>
-            <div className="form-item">
-              <TextField id="create-opt1" variant="filled" label="Option 1" required="true"/>
-            </div>
-            <div className="form-item">
-              <TextField id="create-opt2" variant="filled" label="Option 2" required="true"/>
-            </div>
-            <div className="form-item submit-btn">
-              <Button variant="contained">Submit</Button>
-            </div>
-          </div>
+          </form>
       </div>
     );
   }
