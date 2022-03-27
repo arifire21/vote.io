7e730a5a606491435626ccee7bba434642fe480e
Andrew <andrewscaldwell@gmail.com>
2022-03-27 11:48:51 -0400

    swag server code

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import Grid from '@mui/material/Grid'
 
 export default function Card(props) {
   return (
     // <>
     <div className='vote'>
-
+      <Grid container p={5}>
+      <Grid item xs={6}>
       <div className='voteTitle'>{props.title}</div>
       <div className="voteBody">
         <div className='voteContent'>
           {props.content}
         </div>
       </div>
+      </Grid>
+      <Grid item xs={6}>
+        <img src="/img/vote.png" width="100%"/>
+      </Grid>
+      </Grid>
+      
 
 
     </div>
--- client/src/pages/view-poll.js
+++ client/src/pages/view-poll.js
@@ -8,7 +8,7 @@ import Button from '@mui/material/Button'
 import { useState, useEffect } from 'react'
 import { returnJWT, sign } from '../utils/security'
 import axios from 'axios';
-import {Link,useLocation} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 
 
@@ -18,118 +18,122 @@ import {Link,useLocation} from 'react-router-dom'
 function App() {
 
     const [loaded, setLoaded] = useState(false)
-    const [count,setCount] = useState([])
-  const [entry, setEntry] = useState({
-    created_at: null,
-    description: "",
-    id: "",
-    name: "",
-    type: ""
-  })
-  const [votes,setVotes] = useState([])
+    const [count, setCount] = useState([])
+    const [canVote,setCanVote] = useState(true)
 
-    async function castVote(candidateId){
+    const [entry, setEntry] = useState({
+        created_at: null,
+        description: "",
+        id: "",
+        name: "",
+        type: ""
+    })
+    const [votes, setVotes] = useState([])
+
+    async function castVote(candidateId) {
         const jwt = await returnJWT()
         const instance = axios.create({
             baseURL: 'http://localhost:5000',
             timeout: 10000,
             headers: { 'Authorization': `Bearer ${jwt}` }
-          })
-        
+        })
+
         const signedVote = await sign(candidateId)
         const postable = {
             candidate: candidateId,
             signature: signedVote,
             election: entry.id
         }
-        const {data} = instance.post('/vote',postable)
+        const { data } = await instance.post('/vote', postable)
+        setCanVote(false)
+        await api()
 
     }
 
 
-    const {search:path} = useLocation()
+    const { search: path } = useLocation()
 
-  
 
-  async function api() {
-    
-    const jwt = await returnJWT()
-    if (jwt) {
-      const config = {
-        headers: {
-          'Authorization': `Bearer ${jwt}`
+
+    async function api() {
+
+        const jwt = await returnJWT()
+        if (jwt) {
+            const config = {
+                headers: {
+                    'Authorization': `Bearer ${jwt}`
+                }
+
+            }
+            let query = ''
+            if (path.includes('?id=')) {
+                query = path.split('?id=')[1]
+            }
+            const instance = axios.create({
+                baseURL: 'http://localhost:5000',
+                timeout: 10000,
+                headers: { 'Authorization': `Bearer ${jwt}` }
+            })
+            const { data } = await instance.get(`/election?id=${query}`)
+            const { data: entry } = data
+            const [electionRow] = entry
+            setEntry(electionRow)
+
+            const { data: votesQuery } = await instance.get(`/candidates?election=${electionRow.id}`)
+            const { data: votesArray } = votesQuery
+            setVotes(votesArray)
+
+            const { data: voteCount } = await instance.get('/votes')
+            const { data: votesArrayCount } = voteCount
+            setCount(votesArrayCount)
+
+
         }
 
-      }
-      let query = ''
-      if(path.includes('?id=')){
-        query = path.split('?id=')[1]
-      }
-      const instance = axios.create({
-        baseURL: 'http://localhost:5000',
-        timeout: 10000,
-        headers: { 'Authorization': `Bearer ${jwt}` }
-      })
-      const {data} = await instance.get(`/election?id=${query}`)
-      const {data:entry} = data
-      const [electionRow] = entry
-      setEntry(electionRow)
-
-      const {data:votesQuery} = await instance.get(`/candidates?election=${electionRow.id}`)
-      const {data: votesArray} = votesQuery
-      setVotes(votesArray)
-
-      const {data:voteCount} = await instance.get('/votes')
-      const {data:votesArrayCount} = voteCount
-      setCount(votesArrayCount)
-
-      
     }
 
-  }
-
-  useEffect(() => {
-    api()
-  }, [])
+    useEffect(() => {
+        api()
+    }, [])
 
 
 
 
 
-  return (
-    <div>
-      <Navbar />
+    return (
+        <div>
+            <Navbar />
 
-      <Container maxWidth={1300}>
+            <Container maxWidth={1300}>
 
-        <Grid container spacing={1}  >
-          <Grid item md={11.7}>
-            <Card title={entry.name}
-              content={entry.description}
-            />
-          </Grid>
-          <Grid item md={12}>
-          <h2 class="text-center">Candidates</h2>
-          </Grid>
-          
-          {votes.map(item => (
-          <Subcard subtitle={item.f_name + ' ' + item.l_name}>
-             <p>Affiliation: <strong class="text-white">{item.affiliation}</strong></p>
-             <p class="text-white">VERIFIED VOTES: {count.filter(i => i.candidate_id == item.id).length}</p>
-             <Button onClick={() => castVote(item.id)}>VOTE</Button> 
-          </Subcard>
-          ))}
+                <Grid container spacing={1}  >
+                    <Grid item md={11.7}>
+                        <Card title={entry.name}
+                            content={entry.description}
+                        />
+                    </Grid>
+                    <Grid item md={12}>
+                        <h2 class="text-center">Candidates</h2>
+                    </Grid>
 
-         
-
-        </Grid>
-
-      </Container>
-
-    </div>
+                    {votes.map(item => (
+                        <Subcard subtitle={item.f_name + ' ' + item.l_name}>
+                            <p>Affiliation: <strong class="text-white">{item.affiliation}</strong></p>
+                            <p class="text-white">VERIFIED VOTES: {count.filter(i => i.candidate_id == item.id).length}</p>
+                            <Button onClick={() => castVote(item.id)} disabled={!canVote}><p class="text-white">VOTE</p></Button>
+                        </Subcard>
+                    ))}
 
 
-  );
+
+                </Grid>
+
+            </Container>
+
+        </div>
+
+
+    );
 }
 
 export default App;
