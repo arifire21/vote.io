c3bac6b1108ed4cdcd12f7ba0e5029f1dbcc09da
TRANG DO <dokieuthutrang@gmail.com>
2022-03-27 03:39:15 -0400

    navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,29 +1,54 @@
-import { Button } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import { Link } from "react-router-dom";
+
+import { ImHome} from 'react-icons/im';
+
+import { RiLoginBoxFill} from 'react-icons/ri';
+import { IoMdCreate} from 'react-icons/io';
+import { AiFillEye} from 'react-icons/ai';
 
 export default function Navbar() {
     return (
-        <div className="navbar-container">
+        <div className="navbar">
             <div className="navlink-container">
                 <div className="navlink-item">
-                    <Button variant="text" size="small">
-                        <Link to="/">Home</Link>
-                    </Button>
+                    <div className="navbar-tooltip">
+                        <Tooltip title="Home" placement="right">
+                            <Button variant="text" size="large">
+                                <Link to="/"><ImHome/></Link>
+                            </Button>
+                        </Tooltip>
+                    </div>
                 </div>
+
                 <div className="navlink-item">
-                    <Button variant="text" size="small">
-                        <Link to="/login">Login</Link>
-                    </Button>
+                   <div className="navbar-tooltip">
+                        <Tooltip title="Login" placement="right">
+                            <Button variant="text" size="large">
+                                <Link to="/login"><RiLoginBoxFill/></Link>
+                            </Button>
+                        </Tooltip>
+                    </div>
                 </div>
+
                 <div className="navlink-item">
-                    <Button variant="text" size="small">
-                        <Link to="/create-poll">Create Poll</Link>
-                    </Button>
+                    <div className="navbar-tooltip">
+                        <Tooltip title="Create Poll" placement="right">
+                            <Button variant="text" size="large">
+                                <Link to="/create-poll"><IoMdCreate/></Link>
+                            </Button>
+                        </Tooltip>
+                    </div>
                 </div>
+
                 <div className="navlink-item">
-                    <Button variant="text" size="small">
-                        <Link to="/view-poll">View Polls</Link>
-                    </Button>
+                    <div className="navbar-tooltip">
+                        <Tooltip title="View Polls" placement="right">
+                            <Button variant="text" size="large">
+                                <Link to="/view-poll"><AiFillEye/></Link>
+                            </Button>
+                        </Tooltip>
+                    </div>    
                 </div>
             </div>
         </div>
